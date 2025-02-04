f7f49b4f71bc78a299bccae3484a2079e13a8809
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-02-04 11:15:06 -0400

    FEAT: return null for non string args

--- reverseString.js
+++ reverseString.js
@@ -1,4 +1,8 @@
 const reverseString = function(str) {
+  if (typeof str !== 'string') return null
+
+  if (!str) return '';
+  
   return str.split('').reverse().join('');
 }
 
--- reverseString.test.js
+++ reverseString.test.js
@@ -4,3 +4,14 @@ test('Returns reverse string', () => {
   expect(reverseString('hola')).toBe('aloh');
   expect(reverseString('hasta luego')).toBe('ogeul atsah');
 });
+
+test('Returns null if no arguments given', () => {
+  expect(reverseString()).toBeNull();
+});
+
+test('Return null for non String arguments', () => {
+  expect(reverseString(123)).toBeNull();
+  expect(reverseString(true)).toBeNull();
+  expect(reverseString([])).toBeNull();
+  expect(reverseString({})).toBeNull();
+});
