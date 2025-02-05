3d749fcfdb3c7b6c7775b6a659d29e56cd22ad8d
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-02-04 20:39:14 -0400

    FEAT: succesfully shift a word

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -11,3 +11,13 @@ test('Shift of 3 for a given UPPER case letter of the english alphabet', () => {
   expect(caesarCipher('B', 3)).toBe('E');
   expect(caesarCipher('Z', 3)).toBe('C');
 });
+
+test('Shift of 3 for a given lower case word', () => {
+  expect(caesarCipher('hello', 3)).toBe('khoor');
+  expect(caesarCipher('world', 3)).toBe('zruog');
+});
+
+test('Shift of 3 for a given UPPER case word', () => {
+  expect(caesarCipher('HELLO', 3)).toBe('KHOOR');
+  expect(caesarCipher('WORLD', 3)).toBe('ZRUOG');
+});
