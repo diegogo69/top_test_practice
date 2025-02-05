c80837988e7695ef584288c8c2addcdef497d6f5
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-02-04 20:33:34 -0400

    FEAT: Succesfully shift single UPPER chars

--- caesarCipher.js
+++ caesarCipher.js
@@ -5,41 +5,73 @@ const lettersArr = [
 ];
 
 const lettersObj = {
-    a: 0,
-    b: 1,
-    c: 2,
-    d: 3,
-    e: 4,
-    f: 5,
-    g: 6,
-    h: 7,
-    i: 8,
-    j: 9,
-    k: 10,
-    l: 11,
-    m: 12,
-    n: 13,
-    o: 14,
-    p: 15,
-    q: 16,
-    r: 17,
-    s: 18,
-    t: 19,
-    u: 20,
-    v: 21,
-    w: 22,
-    x: 23,
-    y: 24,
-    z: 25,
-}
+    a: { isUpper: false, index: 0 },
+    b: { isUpper: false, index: 1 },
+    c: { isUpper: false, index: 2 },
+    d: { isUpper: false, index: 3 },
+    e: { isUpper: false, index: 4 },
+    f: { isUpper: false, index: 5 },
+    g: { isUpper: false, index: 6 },
+    h: { isUpper: false, index: 7 },
+    i: { isUpper: false, index: 8 },
+    j: { isUpper: false, index: 9 },
+    k: { isUpper: false, index: 10 },
+    l: { isUpper: false, index: 11 },
+    m: { isUpper: false, index: 12 },
+    n: { isUpper: false, index: 13 },
+    o: { isUpper: false, index: 14 },
+    p: { isUpper: false, index: 15 },
+    q: { isUpper: false, index: 16 },
+    r: { isUpper: false, index: 17 },
+    s: { isUpper: false, index: 18 },
+    t: { isUpper: false, index: 19 },
+    u: { isUpper: false, index: 20 },
+    v: { isUpper: false, index: 21 },
+    w: { isUpper: false, index: 22 },
+    x: { isUpper: false, index: 23 },
+    y: { isUpper: false, index: 24 },
+    z: { isUpper: false, index: 25 },
+
+    A: { isUpper: true, index: 0 },
+    B: { isUpper: true, index: 1 },
+    C: { isUpper: true, index: 2 },
+    D: { isUpper: true, index: 3 },
+    E: { isUpper: true, index: 4 },
+    F: { isUpper: true, index: 5 },
+    G: { isUpper: true, index: 6 },
+    H: { isUpper: true, index: 7 },
+    I: { isUpper: true, index: 8 },
+    J: { isUpper: true, index: 9 },
+    K: { isUpper: true, index: 10 },
+    L: { isUpper: true, index: 11 },
+    M: { isUpper: true, index: 12 },
+    N: { isUpper: true, index: 13 },
+    O: { isUpper: true, index: 14 },
+    P: { isUpper: true, index: 15 },
+    Q: { isUpper: true, index: 16 },
+    R: { isUpper: true, index: 17 },
+    S: { isUpper: true, index: 18 },
+    T: { isUpper: true, index: 19 },
+    U: { isUpper: true, index: 20 },
+    V: { isUpper: true, index: 21 },
+    W: { isUpper: true, index: 22 },
+    X: { isUpper: true, index: 23 },
+    Y: { isUpper: true, index: 24 },
+    Z: { isUpper: true, index: 25 },
+  }
 
 const shiftLetter = function(letter, shift = 3) {
-  const shiftedLetter = (lettersObj[letter] + shift) % 26;
+  const shiftedLetter = (lettersObj[letter].index + shift) % 26;
+
+  if (lettersObj[letter].isUpper) return lettersArr[shiftedLetter].toUpperCase();
+
   return lettersArr[shiftedLetter];
 }
 
 const caesarCipher = function(str, shift) {
-  return str.split('').map((letter) => shiftLetter(letter, shift)).join('');
+  return str.split('')
+    .map((letter) => shiftLetter(letter, shift))
+    .join('');
 }
 
 export default caesarCipher;
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,7 +1,13 @@
 import caesarCipher from "./caesarCipher";
 
-test('Shift of 3 for a given letter of the english alphabet', () => {
+test('Shift of 3 for a given lower case letter of the english alphabet', () => {
   expect(caesarCipher('a', 3)).toBe('d');
   expect(caesarCipher('b', 3)).toBe('e');
   expect(caesarCipher('z', 3)).toBe('c');
+});
+
+test('Shift of 3 for a given UPPER case letter of the english alphabet', () => {
+  expect(caesarCipher('A', 3)).toBe('D');
+  expect(caesarCipher('B', 3)).toBe('E');
+  expect(caesarCipher('Z', 3)).toBe('C');
 });
